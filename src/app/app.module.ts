ef502f8e2619c9a8ba8591e24a7127fa1ad8585a
Selvan Bapu <selvanbapu.jobs@gmail.com>
2021-02-17 00:20:56 +0530

    Added Layout fot the test application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ControlsModule } from './controls/controls.module';
 import { FormAssemblerModule } from './form-assembler/form-assembler.module';
 import { FormBuilderViewModule } from './form-builder-view/form-builder-view.module';
 import { FormRendererModule } from './form-renderer/form-renderer.module';
 import { PreviewModule } from './preview/preview.module';
+import { FormBuilderViewComponent } from './form-builder-view/form-builder-view.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormBuilderViewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    DragDropModule,
     ControlsModule,
     FormAssemblerModule,
     FormBuilderViewModule,
@@ -24,6 +28,7 @@ import { PreviewModule } from './preview/preview.module';
     PreviewModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap: [AppComponent]
+  bootstrap: [FormBuilderViewComponent]
 })
 export class AppModule { }
---
+++ src/app/form-builder-view/form-builder-view.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-form-builder-view',
+  templateUrl: './form-builder-view.component.html',
+  styleUrls: ['./form-builder-view.component.css']
+})
+export class FormBuilderViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
